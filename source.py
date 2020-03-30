6745932fab343fc68307384a495962fe188c32a1
Silviu <silviudinca412@gmail.com>
2020-03-30 15:24:27 +0300

    Problem solved

--- source.py
+++ source.py
@@ -1,4 +1,14 @@
 # Write a method that returns all subsets of a set
+import itertools 
+
 Set = set((1,2,3))
-Set.add(5)
-print (Set)
+
+#return a list of subsets of n lenght
+def Return_nSubsets(n):
+    return list(itertools.combinations(Set,n))
+#print all subsets of set 
+def Print_allSubsets():
+    for i in range(1,len(Set)+1):
+        print(Return_nSubsets(i))
+
+Print_allSubsets()
